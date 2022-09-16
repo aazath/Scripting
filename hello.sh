20ce4a310b42a7a6f99c67619e1b7058f9e37781
aazath <asaazath@gmail.com>
2022-09-16 10:37:43 +0530

    Bash scripting

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo Hello World!
+echo $bash
+echo $BASH_VERSION
+ECHO $HOME
+ECHO $PWD
+name =mark
+
+echo Hello, $name welcome to the Shell Scripting Class 
---
+++ variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+NAME="Aazath"
+SPORT="Soccer"
+echo "My Name is $NAME and my favourite sport is ${SPORT}"
